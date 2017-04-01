543447fb5c543da4cf4204ee7e3ce39336ea60a9
Jonathan Harris <j.wilfred.harris@gmail.com>
2017-03-31 22:41:16 -0300

    Modified pendulum runner to demonstrate functionality of SimplePendulum and RegularPendulum on Earth and Jupiter. This involved dynamically modifying the GravityConstant object associated with each of the SimplePendulum and RegularPendulum.

--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -9,32 +9,55 @@ import java.text.NumberFormat;
 public class PendulumRunner {
 	
 	private final static double EARTH_G = 9.80665;
+	private final static double JUPITER_G = 25.00000;
 
     public static void main (String [] args) {
-	NumberFormat nf = NumberFormat.getInstance ();
-	nf.setMaximumFractionDigits (3);
+    	
+    	NumberFormat nf = NumberFormat.getInstance ();
+    	nf.setMaximumFractionDigits (3);
 
-	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
-	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
-	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, new GravityConstant(EARTH_G), delta);
-	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, new GravityConstant(EARTH_G));
-	RegularPendulum rpCoarse = 
-	    new RegularPendulum (sLen, pMass, theta0, new GravityConstant(EARTH_G), .1);
+    	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
+    	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
+    	
+    	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, new GravityConstant(EARTH_G), delta);
+    	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, new GravityConstant(EARTH_G));
+    	
+    	// print out difference in displacement in 1 second intervals
+    	// for 20 seconds
+    	
+    	int iterations = (int) (1/delta);
+    	System.out.println("Planet Earth:\n");
+    	System.out.println ("analytical vs. numerical displacement (regularPendulum, simplePendulum)");
+    	
+    	for (int second = 1; second <= 20; second++) {
+    		
+    		for (int i = 0; i < iterations; i++) rp.step ();
 
-	// print out difference in displacement in 1 second intervals
-	// for 20 seconds
-	int iterations = (int) (1/delta);
-	System.out.println ("analytical vs. numerical displacement (fine, coarse)");
-	for (int second = 1; second <= 20; second++) {
-	    for (int i = 0; i < iterations; i++) rp.step ();
-	    for (int i = 0; i < 10; i++) rpCoarse.step (); 
-	    System.out.println ("t=" + second + "s: \t" + 
-				nf.format (Math.toDegrees (sp.getTheta (second))) 
-				+ "\t" +
-				nf.format (Math.toDegrees (rp.getLastTheta ()))
-				+ "\t" + 
-				nf.format (Math.toDegrees (rpCoarse.getLastTheta ())));
-	}
+    		System.out.println ("t=" + second + "s: \t" + 
+    		
+    				nf.format (Math.toDegrees (sp.getTheta (second))) 
+					+ "\t" +
+					nf.format (Math.toDegrees (rp.getLastTheta ())));
+    	}
+    	
+    	// Adjust gravitational field dynamically.
+    	sp.setGravitationalField(new GravityConstant(JUPITER_G));
+    	rp.setGravitationalField(new GravityConstant(JUPITER_G));
+    	
+      	System.out.println("\nPlanet Jupiter:\n");
+    	System.out.println ("analytical vs. numerical displacement (regularPendulum, simplePendulum)");
+    	
+    	for (int second = 1; second <= 20; second++) {
+    		
+    		for (int i = 0; i < iterations; i++) rp.step ();
+
+    		System.out.println ("t=" + second + "s: \t" + 
+    		
+    				nf.format (Math.toDegrees (sp.getTheta (second))) 
+					+ "\t" +
+					nf.format (Math.toDegrees (rp.getLastTheta ())));
+    	}
+    	
     }
 }
 
